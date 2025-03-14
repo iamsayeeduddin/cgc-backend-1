b73b53893dbd9b7cbe461aff6f21e3558beb3d8c
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-03-14 08:17:06 +0530

    Express 2

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require("express");
 
 const app = express();
 
+let books = [
+  {
+    id: 1,
+    name: "HTML & CSS",
+    price: 400,
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    price: 800,
+  },
+  {
+    id: 3,
+    name: "React JS",
+    price: 1000,
+  },
+];
+
 app.listen(5000, () => console.log("Server is Up & Running!"));
 
 app.use("/health", (req, res) => {
@@ -9,7 +27,21 @@ app.use("/health", (req, res) => {
   res.send("Welcome to my Express Server!");
 });
 
-app.use("/books", (req, res) => {
+app.get("/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  let book = books.find((value) => value.id === bookId);
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404).send("Book Not found!");
+  }
+});
+
+app.get("/books", (req, res) => {
   res.status(200);
-  res.send("Welcome to my Express Server!");
+  res.json(books);
+});
+
+app.post("/books/create", (req, res) => {
+  console.log(req.body);
 });
